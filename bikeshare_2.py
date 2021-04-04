39541bdddf95b1e715d47098d6953d84eeac00da
Miguel Valencia <miguel_valencia@mckinsey.com>
2021-04-04 12:48:11 -0500

    add args to station_stats

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -71,7 +71,12 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+    Displays statistics on the most popular stations and trip.
+    
+    args:
+        (DataFrame) df - dataframe to calculate statistics
+    """
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
