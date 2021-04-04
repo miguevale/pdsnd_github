25ccc2f29fd39103123bfd741f6fb70b89c60833
Miguel Valencia <miguel_valencia@mckinsey.com>
2021-04-04 12:47:21 -0500

    docs: add args to time_stats

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -46,7 +46,13 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """
+    Displays statistics on the most frequent times of travel.
+    
+    args:
+        (DataFrame) df - dataframe to calculate statistics
+
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
